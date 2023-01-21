8291b14a71427c50581900f85a9ec96236ae43f2
sourav199 <souravdhar1999@gmail.com>
2023-01-21 15:38:01 -0500

    feat:L working with signin

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Signin() {
-  return <div>Signin</div>;
+  const [showPass, SetShowpass] = new useState(false);
+  const [dataForm, setDataform] = new useState({ email: "", pass: "" });
+  const { email, pass } = dataForm;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 }
 
 export default Signin;
