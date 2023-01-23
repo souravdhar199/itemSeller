e3488f45114e1b53dfb9a9ecce7839dd2a7a5c05
sourav199 <souravdhar1999@gmail.com>
2023-01-23 18:28:19 -0500

    feat: Created skeleton for Sign - In

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Signin() {
   const [showPass, SetShowpass] = new useState(false);
@@ -7,9 +7,46 @@ function Signin() {
   const { email, pass } = dataForm;
   const navigate = useNavigate();
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <div>
+        <header>
+          <p>Hi, welcome!</p>
+        </header>
+        <form>
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) =>
+              setDataform((prevState) => ({
+                ...prevState,
+                email: e.target.value,
+              }))
+            }
+          />
+          <div>
+            <input
+              type={showPass ? "text" : "password"}
+              value={pass}
+              onChange={(e) =>
+                setDataform((prevState) => ({
+                  ...prevState,
+                  pass: e.target.value,
+                }))
+              }
+            />
+            <button
+              onClick={(e) => [e.preventDefault(), SetShowpass(!showPass)]}
+            >
+              {showPass ? "Hide Pass" : "Show Pass"}
+            </button>
+          </div>
+          <Link to="/forgot-passward">Forgot passward</Link>
+          <button>Sign In</button>
+        </form>
+        <Link to="/sign-up">Sign Up</Link>
+      </div>
+    </>
   );
 }
 
