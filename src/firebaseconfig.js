7baf7509ffdef39338c9d2f86ae720d2b03107c1
sourav199 <souravdhar1999@gmail.com>
2023-01-25 14:46:01 -0500

    Feat: Registered new user and fixed firebase issue

---
+++ src/firebaseconfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyARUHMt6SizSmZpBkcWUdIyTY14OarMXd0",
+  authDomain: "itemsellerapp.firebaseapp.com",
+  projectId: "itemsellerapp",
+  storageBucket: "itemsellerapp.appspot.com",
+  messagingSenderId: "877007439316",
+  appId: "1:877007439316:web:67afadc7e38821adfbc241",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+
+import { db } from "../firebaseconfig.js";
 
 function Signup() {
   const [showPass, SetShowpass] = new useState(false);
@@ -10,13 +17,40 @@ function Signup() {
   });
   const { name, email, pass } = dataForm;
   const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth(); // getinng auth value
+
+      //registering the user which return a promise
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        pass
+      );
+
+      //then we will get the promise data
+      const user = userCredential.user;
+      console.log(user);
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      navigate("/profile");
+    } catch {
+      console.log("Error");
+    }
+  };
   return (
     <>
       <div>
         <header>
           <p>Hi, welcome!</p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="name"
             placeholder="What is your name"
