681d56e6fcc865b08af5fda5d105f7db1b21bfbb
sourav199 <souravdhar1999@gmail.com>
2023-01-31 16:32:50 -0500

    feat: react nested router and coustom hook

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import useAuthStatus from "../hooks/useAuthStatus";
 
 export default function PrivateRoute() {
-  const dummy = false;
-  return dummy ? <Outlet /> : <Navigate to="/sign-in" />;
+  const { loggedIN, check } = useAuthStatus();
+  if (check) {
+    return <></>;
+  }
+  return loggedIN ? <Outlet /> : <Navigate to="/sign-in" />;
 }
---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+function useAuthStatus() {
+  const [loggedIN, setloggedIN] = useState(false);
+  const [check, setCheck] = useState(true);
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setloggedIN(true);
+      }
+
+      setCheck(false);
+    });
+  });
+  return { loggedIN, check };
+}
+export default useAuthStatus;
