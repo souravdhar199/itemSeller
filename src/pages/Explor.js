3fd0d25417f6b5161cd3449a8d526e3659baf207
sourav199 <souravdhar1999@gmail.com>
2023-02-01 14:41:56 -0500

    fix: Working with Explore page

--- src/pages/Explor.js
+++ src/pages/Explor.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Explor() {
-  return <div>Explor</div>;
+  return (
+    <div>
+      <h1>Welcome to Property Seller , Where you can buy/Sell anything</h1>
+    </div>
+  );
 }
 
 export default Explor;
