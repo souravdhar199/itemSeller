30badbda997eec689a497815b769a1fd71ad3921
sourav199 <souravdhar1999@gmail.com>
2023-02-03 14:08:03 -0500

    feat: designed item list

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useParams } from "react-router-dom";
+import "../CSS/item.css";
 export default function Listing({ data, id }) {
+  const params = useParams();
   return (
-    <div>
-      <img src={data.imgurl} alt="ball" />
-      <p>{data.name}</p>
-      <p>${data.Price}</p>
-      <p>Location: {data.City}</p>
+    <div className="parent1">
+      <Link to={`/catagory/${params.catagoryName}/${id}`}>
+        <div className="imageDiv">
+          {" "}
+          <img src={data.imgurl} alt="ball" />
+        </div>
+        <p>{data.name}</p>
+        <p>${data.Price}</p>
+        <p>Location: {data.City}</p>
+      </Link>
     </div>
   );
 }
